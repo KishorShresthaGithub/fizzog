cfef548e1c682407ed5695c38762308f80031bec
KishorShresthaGithub <shrestha.kishor@grrow.jp>
2024-10-11 14:24:02 +0900

    Add: Animation on hero text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,24 @@ import Logo from "@/components/Logo";
 import Socials from "@/components/Socials";
 import Link from "next/link";
 import DownArrow from "@/assets/icons/DownArrow";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const titleRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    if (window) {
+      const windowWidth = window.innerWidth;
+      let percent = 0;
+
+      window.addEventListener("mousemove", (e) => {
+        const clientX = e.clientX;
+        percent = (clientX / windowWidth) * 100;
+        titleRef.current?.style.setProperty("--fill", `${percent}%`);
+      });
+    }
+  });
+
   return (
     <>
       <Head>
@@ -23,7 +39,9 @@ export default function Home() {
             <Socials />
           </header>
           <section className={styles.hero}>
-            <h1 className={styles.title}>Kishor Shrestha</h1>
+            <h1 className={styles.title} ref={titleRef}>
+              Kishor Shrestha
+            </h1>
           </section>
 
           <footer className={styles.footer}>
